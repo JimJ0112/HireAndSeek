b89007dc5c22fa5517d2debb78da00caf36b0e63
JL Manrique <jimmanrique12@gmail.com>
2022-01-02 18:48:36 +0800

    Update ServicesOffered.js

--- ServicesOffered.js
+++ ServicesOffered.js
@@ -134,7 +134,7 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.send("ReqServiceID=" + rqID);
 
-window.location.replace (availService.html);
+window.location.replace ("availService.html");
 
 
 }
