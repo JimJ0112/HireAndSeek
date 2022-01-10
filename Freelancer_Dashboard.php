0cca731c9db59183bc47fea79f8da49bc681cb18
JL Manrique <jimmanrique12@gmail.com>
2022-01-10 14:15:17 +0800

    Freelancer dashboard layout implemented

--- Freelancer_Dashboard.php
+++ Freelancer_Dashboard.php
@@ -10,7 +10,19 @@ else{
 }
 ?>
 
-<?php
+
+
+<DOCTYPE>
+    <html>
+        <head>
+            <link rel="stylesheet" href="Style.css" type="text/css">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        </head>
+        <body>
+
+            <!-- Nav bar section -->
+            <?php
            
             if(isset($_SESSION['SessionName'])){
 
@@ -21,3 +33,28 @@ else{
                 include_once("Header.php");
             }
             ?>
+            <!-- dashboard pages -->
+            <br> 
+            <nav id="Dashboard_Categories">
+
+    <ul id="Dashboard_CategoriesList">
+    <li class="Dashboard_CategoriesElement" id="Freelancer_jobs" > JOBS </li>
+    <li class="Dashboard_CategoriesElement" id="Freelancerd_transactions" > TRANSACTIONS </li>
+    <li class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > DASHBOARD </li>
+    <li class="Dashboard_CategoriesElement" id="Freelancer_profile" > PROFILE </li>
+  
+    </ul>
+
+        </nav>
+            <br>
+
+        <div id="dashboard_content_grid"> 
+            <div class="dashboard_content"> JOB</div>
+            <div class="dashboard_content"> TRANSACTIONS </div>
+            <div class="dashboard_content"> DASHBOARD </div>
+            <div class="dashboard_content"> PROFILE </div>
+
+        </div>
+
+        </body>
+    </html>
