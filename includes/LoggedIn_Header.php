dfd8262e09c964bc5e378643de5172b1888779e2
JL Manrique <jimmanrique12@gmail.com>
2022-01-17 15:17:11 +0800

    fixed conflict

---
+++ includes/LoggedIn_Header.php
@@ -0,0 +1,41 @@
+<?php
+//session_start();
+
+$sessionID = $_SESSION['SessionName'];
+$accountType = $_SESSION["AccountType"];
+
+if($accountType == "Freelancer"){
+    $redirectQuery = "Freelancer_Dashboard.php";
+
+} else if($accountType == "Customer"){
+    $redirectQuery= "Customer_Dashboard.php";
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="Style.css" type="text/css">
+            <script src="Scripts/Login.js"> </script>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        </head>
+        <body>
+
+            <!-- Nav bar section -->
+            <nav> 
+                <a href="index.php">Home</a>
+                <ul id = "navlist">
+                    <li class= "navElement" id="searchbar"> <input type = "text" >  <input type="button"> </li>
+                    <li class= "navElement"> <a href = "ServicesOffered.php"> EXPLORE </a> </li>
+                    <li class= "navElement"> <a href = "BecomeAFreelancer.php"> BECOME A FREELANCER </a> </li>
+                    <li class= "navElement"> <a href = "About.php"> ABOUT </a> </li>
+                    <li class="navElement"> <a href=<?php echo $redirectQuery;?>> <?php echo $sessionID;?> </a> </li>
+                    <li class="navElement"><a href="Backend/logout.php"> LOG OUT </a> </li>
+                </ul>
+            </nav>
+</body>
+</html>
