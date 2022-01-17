ceb7d15a98a97eeebe0920fb6957c9ea693e7c42
Astozel <97175301+Astozel@users.noreply.github.com>
2022-01-17 23:14:00 +0800

    Header

--- includes/LoggedIn_Header.php
+++ includes/LoggedIn_Header.php
@@ -27,14 +27,26 @@ if($accountType == "Freelancer"){
 
             <!-- Nav bar section -->
             <nav> 
-                <a href="index.php">Home</a>
                 <ul id = "navlist">
-                    <li class= "navElement" id="searchbar"> <input type = "text" >  <input type="button"> </li>
-                    <li class= "navElement"> <a href = "ServicesOffered.php"> EXPLORE </a> </li>
-                    <li class= "navElement"> <a href = "BecomeAFreelancer.php"> BECOME A FREELANCER </a> </li>
-                    <li class= "navElement"> <a href = "About.php"> ABOUT </a> </li>
-                    <li class="navElement"> <a href=<?php echo $redirectQuery;?>> <?php echo $sessionID;?> </a> </li>
-                    <li class="navElement"><a href="Backend/logout.php"> LOG OUT </a> </li>
+                <a href="index.php"><img id="Logo" src="WebsiteImages/Logo.png"></a>
+                    <li class= "navElement" id="searchbar"> <input type = "text" placeholder="Search..  "></li>
+                    <li class= "navElement"> <input type="button" id="btnSearch"> </li>
+                    <li class= "navElement" id="btnExplore"> <a href = "ServicesOffered.php"> EXPLORE </a> </li>
+                    <li class= "navElement" id="btnBecome"> <a href = "BecomeAFreelancer.php"> BECOME A FREELANCER </a> </li>
+                    <li class= "navElement" id="btnAbout"> <a href = "About.php"> ABOUT </a> </li>
+                    <li class="navElement" id="btnUser"> <a href=<?php echo $redirectQuery;?>> <?php echo $sessionID;?> </a> </li>
+                    <li class="navElement" id="btnLogout"><a href="Backend/logout.php"> LOG OUT </a> </li>
+              
+                    <div id="dropdown" class="navElement">
+                       <button class="btnDrop">&#9776;</button>
+                       <div class="dropdown-content">
+                        <li class= "dropElement" id="dropExplore"> <a href = "ServicesOffered.php"><span> EXPLORE </span></a> </li>
+                        <li class= "dropElement" id="dropBecome"> <a href = "BecomeAFreelancer.php"><span> BECOME A FREELANCER</span> </a> </li>
+                        <li class= "dropElement" id="dropAbout"> <a href = "About.php"><span style="color:white;">ABOUT </span></a> </li>
+                        <li class="dropElement" id="dropUser"><a href=<?php echo $redirectQuery;?>><span style="color:white;"> <?php echo $sessionID;?></span></a> </li>
+                        <li class="dropElement" id="dropLogout"><span><a href="Backend/logout.php"> LOG OUT </span></a> </li>
+                        </div>
+                    </div>
                 </ul>
             </nav>
 </body>
