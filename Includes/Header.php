845fa69db3ec9224fc5d9aa2f1e3e1617571621a
JL Manrique <jimmanrique12@gmail.com>
2022-01-17 21:09:27 +0800

    Search Services Feature Started

--- Includes/Header.php
+++ Includes/Header.php
@@ -1,5 +1,6 @@
 <?php
 //session_start();
+include('checkIfExists.php');
 ?>
 <!DOCTYPE html>
     <html>
--- includes/checkIfExists.php
+++ includes/checkIfExists.php
@@ -21,6 +21,22 @@ function checkIfExists(mysqli $conn,$tablename,$auth,$column){
 
 
 
+function searchSimilar(mysqli $conn,$tablename,$title,$column){
+
+    $queryString = "SELECT $column FROM $tablename WHERE $column = '$auth'";
+    $result = mysqli_query($conn,$queryString);
+    $resultCheck = mysqli_num_rows($result);
+    
+    if($resultCheck > 0){
+        while($row = mysqli_fetch_assoc($result)){
+            $FinalResult =  $row[$column];
+        }
+    }
+
+    return $FinalResult;
+
+}
+
 
 
 ?>
