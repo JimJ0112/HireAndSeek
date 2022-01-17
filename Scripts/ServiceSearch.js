e0a34abb08e35e6fa4201c5e6af9f28816975f73
JL Manrique <jimmanrique12@gmail.com>
2022-01-17 22:01:37 +0800

    search function 40% implemented

---
+++ Scripts/ServiceSearch.js
@@ -0,0 +1,21 @@
+function SendSearch(){
+
+    var query = document.getElementById("title").value;
+    var params = "title="+query;
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("POST", "Backend/SearchServices.backend.php", true);
+    
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === 4 || this.status === 200){ 
+            console.log(this.responseText); // echo from php
+     
+       
+        }       
+    };
+    xmlhttp.send(params);
+
+
+}
--- includes/checkIfExists.php
+++ includes/checkIfExists.php
@@ -23,17 +23,24 @@ function checkIfExists(mysqli $conn,$tablename,$auth,$column){
 
 function searchSimilar(mysqli $conn,$tablename,$title,$column){
 
-    $queryString = "SELECT $column FROM $tablename WHERE $column = '$auth'";
+    $queryString = "SELECT $column FROM $tablename WHERE $column = '$title'";
     $result = mysqli_query($conn,$queryString);
     $resultCheck = mysqli_num_rows($result);
-    
+    try{
     if($resultCheck > 0){
         while($row = mysqli_fetch_assoc($result)){
             $FinalResult =  $row[$column];
         }
-    }
+        return $FinalResult;
+    } 
 
-    return $FinalResult;
+
+    
+
+} catch(Exception $e){
+
+    echo "does not exist";
+} 
 
 }
 
