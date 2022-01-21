23c4919667a5aa1dd5198cbb7c53a32e749633e0
JL Manrique <jimmanrique12@gmail.com>
2022-01-21 14:16:27 +0800

    Onclick on search results implemented

--- Scripts/AvailService.js
+++ Scripts/AvailService.js
@@ -1,34 +1,9 @@
 
-/*
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("POST", "Backend/ServiceInfoRequest.php", true);
-
-xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState === 4 || this.status === 200){ 
-        console.log(this.responseText); // echo from php
-        var accountSelected = this.response;
-        accountSelected = JSON.parse(accountSelected);
-        
-        
-        document.getElementsByClassName("AccountName")[0].innerHTML = accountSelected[1];
-        document.getElementsByClassName("AccountLevel")[0].innerHTML = accountSelected[2];
-        document.getElementById("ServiceTitle").innerHTML = accountSelected[3];
-        document.getElementById("totalRatings").innerHTML = accountSelected[4];
-        document.getElementById("AvailService_Description").innerHTML = accountSelected[5];
-        document.getElementById("Buy").innerHTML = accountSelected[6];
-        
-        
-    }       
-};
-xmlhttp.send();
-
-*/
-
 var AvailedService = sessionStorage.getItem('AvailedServiceID');
 
-
+if(!AvailedService){ window.location.href="ServicesOffered.php";
+console.log(AvailedService);
+}
 
 
     //Main
@@ -48,7 +23,7 @@ var AvailedService = sessionStorage.getItem('AvailedServiceID');
             setValues(AvailedServiceINFO);
 
             
-        }       
+        } else{window.location.href="ServicesOffered.php"}      
     };
     
      xmlhttp.send("ReqServiceID=" + AvailedService);
@@ -71,6 +46,7 @@ function setValues(array){
     const Service3StarRatings = document.getElementById("3starRatingsTotal");
     const Service2StarRatings =  document.getElementById("2starRatingsTotal");
     const Service1StarRatings = document.getElementById("1starRatingsTotal");
+    const PlanBasic = document.getElementById("Plan_Basic");
     // total ratings (not yet final)
     var ComputedTotalRatings = (1*parseFloat(array[0]['Service5StarRatings'])+2 * parseFloat(array[0]['Service4StarRatings'])+3* parseFloat(array[0]['Service3StarRatings'])+4* parseFloat(array[0]['Service2StarRatings'])+5* parseFloat(array[0]['Service1StarRatings']))/5;
   console.log(ComputedTotalRatings);
@@ -88,6 +64,9 @@ function setValues(array){
    Service2StarRatings.innerText =  array[0]['Service2StarRatings'];
    Service1StarRatings.innerText =  array[0]['Service1StarRatings'];
 
+
+   // NOT DONE YET
+   
     //totalRatings.innerHTML = ;
     
     //console.log(AccountName);
