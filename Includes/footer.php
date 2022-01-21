09e186ac7edc069db9e5ab0e665cae35a035fc2f
Astozel <97175301+Astozel@users.noreply.github.com>
2022-01-22 07:24:57 +0800

    update sa footer ulit

--- Includes/footer.php
+++ Includes/footer.php
@@ -12,16 +12,16 @@
     
          <!-- Footer -->
          <div class="footer">
-               <div class="LeftFooter"> 
+               <div class="leftFooter"> 
                    <div class="txtFooterTitle"><a href="ServicesOffered.php">Service Offered</a></div>
                    <div class="txtFooter"><a href=""> High Rated Services</a></div>
                    <div class="txtFooter"><a href=""> Graphics and Design</a></div>
                    <div class="txtFooter"><a href=""> Video and Presentation</a></div>
                 </div>
-               <div class="CenterFooter">
+               <div class="centerFooter">
                    <div class="txtFooter"><a href=""> Music and Audio</a></div>
                    <div class="txtFooter"><a href=""> Development and IT</a></div></div> 
-                <div class="RightFooter">
+                <div class="rightFooter">
                    <div class="txtFooterTitle"><a href="About.php"> About</a></div>
                    <div class="txtFooter"><a href=""> Team Members</a></div>
                    <div class="txtFooter"><a href=""> Team Overview</a></div></div>    
