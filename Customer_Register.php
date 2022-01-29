a48c54d094868b4f5209028eb5fe82ddcb6462e4
Astozel <97175301+Astozel@users.noreply.github.com>
2022-01-29 21:22:42 +0800

    Register -Done

--- Customer_Register.php
+++ Customer_Register.php
@@ -18,106 +18,143 @@ $data = $_GET['data'];
 
         </head>
         <body>
-
-            <!-- Nav bar section -->
+<div class="Freelancer-Container">
+           <!-- Nav bar section -->
             <?php
             session_start();
             session_destroy();
             session_unset();
-            include_once("includes/Header.php");
+            // include_once("includes/Header.php");
             ?>
+      
+        <div class="Freelancer-Header">
+            <div class="Freelancer-HeaderTitle">CREATE AN ACCOUNT AS A CUSTOMER</div>
+            <div class="Freelancer-HeaderText">COMPLETE YOUR PROFILE BY FILLING UP THIS ACCOUNT CREATION FORM</div>
+        </div>
+
+        <div class="Freelancer-Content">
             <!-- Php form-->  
 
             <div id="LoginForm">
                
             <form action="Backend/Customer_Register.backend.php" method="post" enctype = "multipart/form-data">
-                <br> <br>
             <div id="Register_Layer1">
+            <div class="Layer-Already">Already have an account? <a href="Login_Index.php">Sign-in &#8594</a></div>
+            <div class="Layer-Title">PERSONAL INFORMATION </div>
+            <div class="Layer-div1">
+                    <div class="div-input"><input type="email" name="Email" placeholder="Email" required></div>
+                    <div class="div-input guide" data-tooltip="This is where your future files will be sent."><input type="email" name="FileEmail" placeholder="Files Email Address" required></div>
+                    <div class="div-input"><input type="text" name="UserName" placeholder="Username"></div>
+                    <div class="div-input check"><input type="password" name="UserPassword" placeholder="Password" required></div>
+                    <div class="div-input"><input type="password" placeholder="Confirm Password" required></div>
+                </div>
 
-            <input type="text" name="Email" placeholder="Email"> <br>  
-            <input type="text" name="UserName" placeholder="User Name "> <br>  
-            <input type="password" name="UserPassword" placeholder="Password"> <br> 
-            <input type="password" placeholder=" Confirm Password"> <br> <br> 
+            <div class="Layer-div2">
+                <div class="div-input">
+                    <input type="text" name="FirstName" placeholder="First Name" required> 
+                    <input type="text" name="MiddleInitial" placeholder="Middle Name">
+                    <input type="text" name="LastName" placeholder="Last Name" required> 
+                </div>
+                
+                <div class="div-input">
+                    <input type="text" name="Birthdate" placeholder="Birthday">
+                    <input type="text" name="Contact" placeholder="Contact Number">
+               
+                    <select name="Gender">
+                        <option value="" disabled selected hidden> GENDER </option> 
+                        <option value="male"> Male </option>
+                        <option value="female"> Female </option>
+                    </select>
+                </div>    
+                
+                <div class="div-input">
+                <input type="text" name="Address" placeholder="Address"> 
+                <input type="text" name="Description" placeholder="Description"> 
+                <input type="text" name="Language" placeholder="Language">
+                </div>
+                
+                
+            </div>
 
-            <input type="text" name="FileEmail" placeholder="Files Email Address"> <br>
-            <input type="text" name="FirstName" placeholder="FIRST NAME" required> 
-            <input type="text" name="LastName" placeholder="LAST NAME" required> 
-            <input type="text" name="MiddleInitial" placeholder="MIDDLE NAME"> <br> 
-            <input type="text" name="Birthdate" placeholder="BIRTH DATE"> <br> 
-            <select name="Gender">  <br> 
-                <option value="" disabled selected hidden> GENDER </option> 
-            <option value="male"> MALE </option>
-            <option value="female"> FEMALE </option>
-            </select>
-
-            <br><br>
-
-            
-            <input type="text" name="Contact" placeholder="Contact Number"> <br>  
-            <input type="text" name="Address" placeholder="Address"> <br>  
-
-            
-          
-            
-            <br> <br>
-
-            <input type="text" name="Description" placeholder="Description"> <br> 
-            <input type="text" name="Language" placeholder="Language"> <br> 
-
-</div>
+        </div>
 
 <div id="Register_Layer2">
-
-            <select name="IDTYPE"> 
-                <option value="" disabled selected hidden required> ID TYPE </option>
-            <option value="Driving License"> Driving License </option>
-            <option value="National ID"> National ID </option>
-            <option value="School ID"> School ID </option>
+        <div class="Layer-Already">Already have an account? <a href="Login_Index.php">Sign-in &#8594</a></div>
+        <div class="Layer-Title">VERIFICATION </div>
+        
+        <div class="div-inputUpload1">
+            <select name="IDTYPE" required> 
+                <option value="" disabled selected hidden> ID TYPE </option>
+                <option value="Driving License"> Driving License </option>
+                <option value="National ID"> National ID </option>
+                <option value="School ID"> School ID </option>
             </select>
-
-           <br><br>
-           UPLOAD ID <br>
-        <input type="file" name="IDFile" placeholder="ID " accept=".jpg,.png">
-
-            <br> <br>
-
-            <br> <br>
+            <div class="div-upload" title="Upload FIle">
+                <input type="file" name="IDFile" placeholder="ID" accept=".jpg,.png" id="file1"required>
+                <label for="file1" id="file1-Preview">
+                    <img src="WebsiteImages/Placeholder.png">
+                    <div><span>+</span></div>
+                </label>
+            </div>
+        </div>
+       
+        <div class="div-inputUpload2">  
             <select name="IDTYPE2" required> 
                 <option value="" disabled selected hidden> ID TYPE </option>
-            <option value="Driving License"> Driving License </option>
-            <option value="National ID"> National ID </option>
-            <option value="School ID"> School ID </option>
+                <option value="Driving License"> Driving License </option>
+                <option value="National ID"> National ID </option>
+                <option value="School ID"> School ID </option>
             </select>
-
-           <br><br>
-           UPLOAD ID <br>
-        <input type="file" name="IDFile2" placeholder="ID" accept=".jpg,.png">
+            <div class="div-upload" title="Upload FIle">
+                <input type="file" name="IDFile2" placeholder="ID" accept=".jpg,.png" id="file2"required>
+                <label for="file2" id="file2-Preview">
+                    <img src="WebsiteImages/Placeholder.png">
+                    <div><span>+</span></div>
+                </label>
+            </div>
+        </div>
+        
 </div>
 <div id="Register_Layer3">
 
-<input type="hidden"  id="UserSnapshot" name ="UserSnapshot">     
-
-<canvas id="photocapture"> </canvas>
-    <video autoplay id="video"> </video>
-   
+    <div class="Layer-Already">Already have an account? <a href="Login_Index.php">Sign-in &#8594</a></div>
+    <div class="Layer-Title">VERIFICATION  </div>
+    <input type="hidden"  id="UserSnapshot" name ="UserSnapshot">     
+    <div class="Layer3-Content">
+    <div class="Layer-Photo"><canvas id="photocapture"> </canvas></div>
+    <div class="Layer-Video"><video autoplay id="video"> </video></div>
+    </div>
+        
 </div>
 <div id="Register_Layer4">
-    <h3>  Terms and Conditions Placeholder</h3>
-            <input type="submit" value="Sign Up" >
-</div>  
-            </form>
-            <br> 
-            <button onclick="startvideo()" id="startvideo"> Open Camera </button>
-            <button  id="CaptureImage"> Capture </button>
-            <br>
-            <button onclick="displayNext()" id="Next"> NEXT </button>
-            </div>
-        
-            <br> <br>
+    <div class="Layer-Already">Already have an account? <a href="Login_Index.php">Sign-in &#8594</a></div>
+    <div class="Layer-Title">TERMS AND CONDITIONS </div>
+   
+    <div class="Layer4-Form">
+        <h3>  Please read and agree before we proceed.</h3>
+        <div>I agree to the collection and use of the data that I have provided to HireAndSeek 
+            through this registration form for the use of this website. 
+            I understand that the collection and use of this data, which may include personal 
+            information and sensitive personal information, shall be in accordance with the 
+            Data Privacy Act of 2012 and the Privacy Policy of HireAndSeek.</div>
+        <input type="checkbox" id="checkboxAccept" required>
+        <label for="checkboxAccept">Accept Terms and Condition</label>
+        <div class="Layer4Btn"><button type="submit">Sign Up</button></div>
+    </div>
+</div>
+
+</form>
+                <div class="Layer3Btn">
+                <button onclick="startvideo()" id="startvideo"> Open Camera </button>
+                <button  id="CaptureImage"> Capture </button>
+                </div>
+</div>  <!-- Close login form -->
+</div> <!-- content  -->
+<div class="nextBtn"><button onclick="displayNext()" id="Next"> NEXT &#8594 </button></div>
 
         <!-- Footer -->
         <?php include_once('Includes/footer.php')?>
-
+</div>
             
             <script src="Scripts/Register.js"> </script>
         </body>
