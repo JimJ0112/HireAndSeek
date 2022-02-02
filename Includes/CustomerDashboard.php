746d014017c375531a16b9fc33f3c5b7f2ed19b7
Astozel <97175301+Astozel@users.noreply.github.com>
2022-02-02 19:30:23 +0800

    Profile / Message

--- Includes/CustomerDashboard.php
+++ Includes/CustomerDashboard.php
@@ -6,24 +6,24 @@
 
 <html>
     <head>
-    <link rel="stylesheet" href="../Style.css" type="text/css">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"
-</head>
+    <link rel="stylesheet" href="Style.css" type="text/css">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
 
 <body>
 
-<nav id="Dashboard_Categories">
+<div id="Dashboard_Categories">
 
-<ul id="Dashboard_CategoriesList">
+<!-- <ul id="Dashboard_CategoriesList"> -->
 
-<li class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="../Messages.php"> Messages </a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="../CustomerOrders.php"> ORDERS </a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancerd_transactions" > <a href="../CustomerCanceledOrders.php"> Canceled Orders </a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancer_performance" > <a href="../CustomerDeliveredOrders.php">Delivered Orders </a></li>
-<li class="Dashboard_CategoriesElement" id="Freelancer_profile" > <a href="../ManageProfile.php">  PROFILE </a> </li>
+<div class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="Messages.php"> MESSAGES </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="CustomerOrders.php"> ORDERS </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancerd_transactions" > <a href="CustomerCanceledOrders.php"> CANCELED ORDERS </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_performance" > <a href="CustomerDeliveredOrders.php">DELIVERED ORDERS </a></div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_profile" > <a href="ManageProfile.php">  PROFILE </a> </div>
 
-</ul>
+<!-- </ul> -->
 
-    </nav>
+</div>
 </body>
     </html>
--- Includes/FreelancerDashboard.php
+++ Includes/FreelancerDashboard.php
@@ -6,26 +6,25 @@
 
 <html>
     <head>
-    <link rel="stylesheet" href="../Style.css" type="text/css">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"
+    <link rel="stylesheet" href="Style.css" type="text/css">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
 
-<nav id="Dashboard_Categories">
+<div id="Dashboard_Categories">
 
-<ul id="Dashboard_CategoriesList">
-<li class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="../CreateJob.php"> Add Job </a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="../Messages.php"> Messages </a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="../Orders.php"> ORDERS </a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancer_jobs" > <a href="../Jobs.php"> JOBS <a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancerd_transactions" > <a href="../CanceledOrders.php"> Canceled Orders </a> </li>
-<li class="Dashboard_CategoriesElement" id="Freelancer_performance" > <a href="../DeliveredOrders.php">Completed </a></li>
+<!-- <ul id="Dashboard_CategoriesList"> -->
+<div class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="CreateJob.php"> ADD JOB </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="Messages.php"> MESSAGES </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_dashboard" > <a href="Orders.php"> ORDERS </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_jobs" > <a href="Jobs.php">MY JOBS </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancerd_transactions" > <a href="CanceledOrders.php"> CANCELLED ORDERS </a> </div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_performance" > <a href="DeliveredOrders.php">COMPLETED </a></div>
+<div class="Dashboard_CategoriesElement" id="Freelancer_profile" > <a href="ManageProfile.php">  PROFILE </a> </div>
 
-<li class="Dashboard_CategoriesElement" id="Freelancer_profile" > <a href="../ManageProfile.php">  PROFILE </a> </li>
+<!-- </ul> -->
 
-</ul>
-
-    </nav>
+    </div>
 </body>
     </html>
