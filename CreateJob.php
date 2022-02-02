f566565fb60788f49019a24245724950721aca69
Astozel <97175301+Astozel@users.noreply.github.com>
2022-02-02 21:33:59 +0800

    Add Job

--- CreateJob.php
+++ CreateJob.php
@@ -29,7 +29,7 @@ if($_SESSION["AccountType"] != "Freelancer"){
         <body>
 
 
-<div class="Login-Container">
+<div class="CreateJob-Container">
             <!-- Nav bar section -->
         <div class="nav">
 
@@ -50,52 +50,42 @@ if($_SESSION["AccountType"] != "Freelancer"){
                include_once("includes/CustomerDashboard.php");
            }
 
-
-
-
            ?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </div>
-            <div class="logForm">
+
+            <div class="CreateJob-Content">
                  <form action="Backend/CreateJobBackend.php" method="post" enctype = "multipart/form-data">
-                    
-                         <select name="Category">  <br> 
-                         <option value="" disabled selected hidden> Category </option> 
-                         <option value="Graphics and Design"> Graphics and Design </option>
-                         <option value="Video and Presentation"> Video and Presentation </option>
-                         <option value="Music and Audio"> Music and Audio </option>
-                         <option value="Development and IT"> Development and IT </option>
-                         </select>  
-                        
-                        <div class="logDiv"><input type="text" name="JobName" placeholder="Job Name" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="JobDescription" placeholder="Job Description" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="BasicPlanDescription" placeholder="Job Basic Plan Description" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="StandardPlanDescription" placeholder="Job Standard Plan Description" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="PremiumPlanDescription" placeholder="Job Premium Plan Description" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="BasicPlanPrice" placeholder="Job Basic Plan Price" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="StandardPlanPrice" placeholder="Job Standard Plan Price" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="PremiumPlanPrice" placeholder="Job Premium Price" class="logTB"></div>
-                        <div class="logDiv"><input type="text" name="GcashNumber" placeholder="G-Cash Account Number" class="logTB"></div>
-                        
-                        <div id="UploadFiles">
-                        <input type="file" name="Banner1" accept=".jpg,.png">
-                        <input type="file" name="Banner2" accept=".jpg,.png">
-                        <input type="file" name="Banner3" accept=".jpg,.png">
+                        <div class="CreateJob-Category">
+                            <select name="Category" >
+                            <option value="" disabled selected hidden> Category </option> 
+                            <option value="Graphics and Design"> Graphics and Design </option>
+                            <option value="Video and Presentation"> Video and Presentation </option>
+                            <option value="Music and Audio"> Music and Audio </option>
+                            <option value="Development and IT"> Development and IT </option>
+                            </select>  
+                         </div>
+                        <div class="CreateJob-Inputs">
+                            <div class="div-input">
+                            <input type="text" name="JobName" placeholder="Job Name" class="logTB">
+                            <input type="text" name="JobDescription" placeholder="Job Description" class="logTB">
+                            <input type="text" name="GcashNumber" placeholder="G-Cash Account Number" class="logTB">
+                            </div>
+                            <div class="div-input">
+                            <input type="text" name="BasicPlanDescription" placeholder="Job Basic Plan Description" class="logTB">
+                            <input type="text" name="StandardPlanDescription" placeholder="Job Standard Plan Description" class="logTB">
+                            <input type="text" name="PremiumPlanDescription" placeholder="Job Premium Plan Description" class="logTB">
+                            </div>
+                            <div class="div-input">
+                            <input type="text" name="BasicPlanPrice" placeholder="Job Basic Plan Price" class="logTB">
+                            <input type="text" name="StandardPlanPrice" placeholder="Job Standard Plan Price" class="logTB">
+                            <input type="text" name="PremiumPlanPrice" placeholder="Job Premium Price" class="logTB">
+                            </div>
+                       </div>
+                        <div class="UploadFiles">
+                            <input type="file" name="Banner1" accept=".jpg,.png">
+                            <input type="file" name="Banner2" accept=".jpg,.png">
+                            <input type="file" name="Banner3" accept=".jpg,.png">
                         </div>
            <!--
         <div id="UploadFiles">
@@ -131,8 +121,8 @@ if($_SESSION["AccountType"] != "Freelancer"){
 
 
         -->
-                    <div class="logDiv">
-                        <input type="submit" value="CREATE JOB" class="logBtnSubmit">
+                    <div class="CreateJobBtn">
+                        <input type="submit" value="CREATE JOB" class="CJBtn">
                     </div>
                    
                     
